88aa7ee4047445838793cdf7d9536d79b24afd07
pinar1987 <pinarbildirici87@gmail.com>
2025-05-05 19:18:45 +0200

    user schema done

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ mongoose.connect("mongodb://127.0.0.1:27017/mynewdatabase")
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
+const userSchema = new mongoose.Schema({
+  first_name: String,
+  last_name: String,
+  email: String
+});
+
+const User = mongoose.model("User", userSchema);
 
 const PORT = 3000;
 app.listen(PORT, () => {
